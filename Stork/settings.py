9a0fc5b8694bcb3fd0d2a6310e6a5d48e9ae9afd
salvable <salvable@kakao.com>
2021-06-04 15:58:11 +0900

    key값을 환경변수로 관리

--- Stork/settings.py
+++ Stork/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ke)+@gp^j4cf!dbpq58t-8w@0tlw9b_3+y7c!-#%k-ip_si4ro'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
