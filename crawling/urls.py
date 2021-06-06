bea26d07ab05c1ea8a6c5bad50c7b2907bdf8728
salvable <salvable@kakao.com>
2021-06-06 20:28:03 +0900

    parameter test code 작성

---
+++ crawling/urls.py
@@ -0,0 +1,11 @@
+# https://cholol.tistory.com/468?category=739855 참고
+
+
+# #urls.py
+from django.conf.urls import url, include
+from django.urls import path
+from crawling import views
+
+urlpatterns = [
+    path('<str:string>/', views.crawling)
+]
