5ad5a95ae7e857abe7ca8eeb2a428be152368bf0
salvable <salvable@kakao.com>
2021-06-07 19:50:48 +0900

    엑셀파일 추가, stork model pk설정

--- crawling/models.py
+++ crawling/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Stork(models.Model):
-    no = models.TextField
+    no = models.TextField(primary_key=True)
     name = models.CharField(max_length=10)
     created = models.DateTimeField(auto_now_add=True)
 
