b361185725c03c8ad95d4bb3b9f7683957537d69
salvable <salvable@kakao.com>
2021-06-08 19:25:07 +0900

    .

---
+++ crawling_stork/admin.py
@@ -0,0 +1,6 @@
+# from django.contrib import admin
+#
+# # Register your models here.
+# from crawling.models import Stork
+#
+# admin.site.register(Stork)
---
+++ db_setting.py
@@ -0,0 +1,14 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_stork',
+        'USER': os.environ.get("DB_USERNAME"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT")
+    }
+}
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
