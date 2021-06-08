0b7a03718e7b2999e079bf7c78ba75a4c980f856
salvable <salvable@kakao.com>
2021-06-08 19:30:47 +0900

    database연동중..

--- Stork/settings.py
+++ Stork/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import db_setting
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,12 +78,7 @@ WSGI_APPLICATION = 'Stork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = db_setting.DATABASES
 
 
 # Password validation
--- crawling_stork/admin.py
+++
@@ -1,6 +0,0 @@
-# from django.contrib import admin
-#
-# # Register your models here.
-# from crawling.models import Stork
-#
-# admin.site.register(Stork)
