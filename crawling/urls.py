34bc9ffd5329b507cd45e01a357863baaea0bd71
salvable <salvable@kakao.com>
2021-06-09 18:01:43 +0900

    crawling 라우팅 추가

--- crawling/urls.py
+++ crawling/urls.py
@@ -8,5 +8,6 @@ from crawling import views
 
 urlpatterns = [
     # addStorks는 DB에 .xlsx 정보를 가져오기위해 최초 한번만 실행할 것
-    path('addStorks/', views.addStorks)
+    path('addStorks/', views.addStorks),
+    path('stork/<str:name>', views.getStork)
 ]
