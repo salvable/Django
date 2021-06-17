6b7365685e818b1b264f555b93af8d8e9dea2b1c
salvable <salvable@kakao.com>
2021-06-17 15:38:07 +0900

    bitcoin 모델 정의

--- crawling/models.py
+++ crawling/models.py
@@ -8,3 +8,8 @@ class Stork(models.Model):
 
     class Meta:
         ordering = ['created']
+
+class Bitcoin(models.Model):
+    market = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=50)
+    eng_name = models.CharField(max_length=50)
